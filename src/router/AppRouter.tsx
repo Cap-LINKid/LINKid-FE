b6d121ee9b90de1ed7facfceb744b6019dadd22a
wldy4627 <wldy4627@gmail.com>
2025-11-08 23:59:26 +0900

    feat(#2): 페이지 라우터 변경 및 Login 페이지 연결

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,45 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { ROUTES } from "./routes";
-import ProtectedRoute from "./ProtectedRoute";
-import {
-    LoginPage,
-    SignupPage,
-    GuidePage,
-    DashboardPage,
-    UploadPage,
-    AnalaysisLoadingPage,
-    ReportStepPage,
-    ReportPage,
-    ReportListPage,
-    ChallengeListPage,
-    ChallengeDetailPage,
-    MyPage,
-} from "../pages";
+import { createBrowserRouter } from "react-router-dom";
+import PlainLayout from "../components/layout/PlainLayout";
+import LoginPage from "../pages/Auth/LoginPage";
 
-const AppRouter = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/* 비로그인 접근 가능 */}
-                <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-                <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
-
-                {/* 로그인 필요 */}
-                <Route element={<ProtectedRoute />}>
-                    <Route path={ROUTES.DASHBOARD} element={<DashboardPage />} />
-                    <Route path={ROUTES.GUIDE} element={<GuidePage />} />
-                    <Route path={ROUTES.UPLOAD} element={<UploadPage />} />
-                    <Route path={ROUTES.ANALYSIS_LOADING} element={<AnalaysisLoadingPage />} />
-                    <Route path={ROUTES.REPORT_STEP(":id")} element={<ReportStepPage />} />
-                    <Route path={ROUTES.REPORT(":id")} element={<ReportPage />} />
-                    <Route path={ROUTES.REPORT_LIST} element={<ReportListPage />} />
-                    <Route path={ROUTES.CHALLENGE_LIST} element={<ChallengeListPage />} />
-                    <Route path={ROUTES.CHALLENGE_DETAIL(":id")} element={<ChallengeDetailPage />} />
-                    <Route path={ROUTES.MYPAGE} element={<MyPage />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );
-};
-
-export default AppRouter;
+export const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
+    {
+        element: <PlainLayout />,
+        children: [
+            { path: "/", element: <LoginPage /> },
+        ],
+    },
+]);
