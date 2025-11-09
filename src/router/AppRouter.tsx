a54e853be7a5037b9fe61945f6f482aab2743ee2
wldy4627 <wldy4627@gmail.com>
2025-11-09 15:18:31 +0900

    feat(#3): Dashboard 페이지 완성 및 라우팅

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import PlainLayout from "../components/layout/PlainLayout";
+import Layout from "../components/layout/Layout";
 import LoginPage from "../pages/Auth/LoginPage";
 import SignupPage from "../pages/Auth/SignupPage";
+import DashboardPage from "../pages/Dashboard/DashboardPage";
 
 export const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
     {
@@ -9,6 +11,12 @@ export const router: ReturnType<typeof createBrowserRouter> = createBrowserRoute
         children: [
             { path: "/", element: <LoginPage /> },
             { path: "/signup", element: <SignupPage /> },
+        ],
+    },
+    {
+        element: <Layout />,
+        children: [
+            { path: "/dashboard", element: <DashboardPage /> },
         ],
     },
 ]);
