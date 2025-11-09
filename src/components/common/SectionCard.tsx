a25a8bee5372e749802aa7aac6bccc0a0f548978
wldy4627 <wldy4627@gmail.com>
2025-11-09 15:17:33 +0900

    feat(#3): Dashboard-SectionCard 및 ReportSection 구현

---
+++ src/components/common/SectionCard.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import type { ReactNode } from "react";
+
+interface SectionCardProps {
+    icon?: ReactNode;
+    title: string;
+    children: ReactNode;
+}
+
+const SectionCard = ({ icon, title, children }: SectionCardProps) => {
+    return (
+        <Wrapper>
+            <Header>
+                {icon && <IconWrapper>{icon}</IconWrapper>}
+                <Title>{title}</Title>
+            </Header>
+            <Content>{children}</Content>
+        </Wrapper>
+    );
+};
+
+export default SectionCard;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 20px 18px;
+    margin-top: 20px;
+    background-color: white;
+    border-radius: ${({ theme }) => theme.radius.xl};
+    box-shadow: 0px 4px 4px rgba(222, 216, 208, 0.2);
+    gap: 8px;
+`;
+
+const Header = styled.div`
+    display: flex;
+    gap: 5px;
+`;
+
+const IconWrapper = styled.div``
+
+const Title = styled.p`
+    font-size: 20px;
+    font-weight: ${({ theme }) => theme.typography.weights.bold};
+`;
+
+const Content = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
---
+++ src/pages/Dashboard/components/ReportSection.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import SectionCard from "../../../components/common/SectionCard";
+import ChartIcon from "../../../assets/icons/chart.svg?react";
+
+const ReportSection = () => {
+    return (
+        <Wrapper>
+            <SectionCard
+                icon={<ChartIcon />}
+                title="나의 성장 리포트"
+            >
+                <SubCard>
+                    <SubTitle>종합 상호작용 점수(QI Score) 변화</SubTitle>
+                    <Description>75점을 달성했어요! 꾸준히 성장하고 있습니다.</Description>
+                    {/* <QIScoreChart /> */}
+                </SubCard>
+                <SubCard>
+                    <SubTitle>긍정/지시적 상호작용(PI/NDI) 변화</SubTitle>
+                    <Description>긍정(PI)는 높을수록, 지시적(NDI) 상호작용은 낮을수록 건강한 균형입니다.</Description>
+                    {/* <PINDIChart /> */}
+                </SubCard>
+            </SectionCard>
+        </Wrapper>
+    );
+};
+
+export default ReportSection;
+
+const Wrapper = styled.div`
+    width: 100%;
+`;
+
+const SubCard = styled.div`
+    border: 1px solid ${({ theme }) => theme.colors.gray[500]};
+    border-radius: ${({ theme }) => theme.radius.md};
+    padding: 14px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+
+const SubTitle = styled.p`
+    font-size: 16px;
+    font-weight: 600;
+`
+
+const Description = styled.p`
+    font-size: 13px;
+    font-weight: 400;
+`
+
+
