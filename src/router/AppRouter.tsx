4e610c53418f2aa47ef75447bb6def0019bf0521
wldy4627 <wldy4627@gmail.com>
2025-11-18 01:05:33 +0900

    feat/#11: Report Step별 페이지 연결 및 라우팅

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -7,6 +7,7 @@ import DashboardPage from "../pages/DashboardPage";
 import GuidePage from "../pages/GuidePage";
 import VideoUploadPage from "../pages/VideoUploadPage";
 import AnalysisPage from "../pages/AnalysisPage";
+import ReportStepPage from "../pages/ReportStepPage";
 
 export const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
     {
@@ -23,6 +24,7 @@ export const router: ReturnType<typeof createBrowserRouter> = createBrowserRoute
             { path: "/guide", element: <GuidePage /> },
             { path: "/upload", element: <VideoUploadPage /> },
             { path: "/analysis", element: <AnalysisPage analysisId="analyzeId" /> },
+            { path: "/report/:reportId/step/:step", element: <ReportStepPage /> },
         ],
     },
 ]);
